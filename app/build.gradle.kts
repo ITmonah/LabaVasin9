a2d8d08bc2b4a066bd9629936b54ead9f788895d
Vasin-AS <vasin.sasha26@gmail.com>
2024-11-19 10:11:50 +0500

    fix google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.labavasin9"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.labavasin9"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
